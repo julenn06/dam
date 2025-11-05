3073f751a18ab01f33f824a612fa28be1196968f
Julen-Elorrieta <julen.fernandezlo@elorrieta-errekamari.com>
2025-11-05 12:32:58 +0100

    Todo lo Aprendido en 1 y 2 de DAM

--- DAM2/Desarrollo de Interfaces/1. Eval/heroes_julen/src/app/pages/homepage/home-page.ts
+++ DAM2/Desarrollo de Interfaces/1. Eval/heroes_julen/src/app/pages/homepage/home-page.ts
@@ -1,19 +1,8 @@
-import {
-  Component
-} from '@angular/core';
-import {
-  CommonModule
-} from '@angular/common';
-import {
-  Router,
-  RouterModule
-} from '@angular/router';
-import {
-  Hero
-} from '../../hero/hero';
-import {
-  allHeroes
-} from '../../hero/hero';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { Hero } from '../../hero/hero';
+import { HeroesRemoteService } from '../../services/heroes-remote-service';
 
 @Component({
   selector: 'app-home-page',
@@ -22,10 +11,20 @@ import {
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {
-  topHeroes: Hero[] = allHeroes.slice(0, 4);
+export class HomePage implements OnInit {
+  topHeroes: Hero[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private remote: HeroesRemoteService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.remote.getFeaturedHeroes(4).subscribe({
+      next: (hs) => {
+        this.topHeroes = hs;
+        this.cdr.markForCheck();
+      },
+      error: (err) => console.error('Failed to load top heroes', err)
+    });
+  }
 
   goToHeroesPage() {
     this.router.navigate(['/heroes']);
